6e7789375c7eb30ba64ff9150fdb5ae18218ccfb
Matthew Wasbrough <matthew.was@gmail.com>
2016-02-10 14:07:05 +0000

    changed icons and implemented js insert for contacts and navigation

---
+++ live/common.js
@@ -0,0 +1,8 @@
+$(function () {
+    'use strict';
+    
+    $("#navigation").html('<ul class="nav navbar-nav"><li id="about"><a href="about.html">About Me</a></li><li id="webdev"><a href="webdev.html">Web Development</a></li><li id="science"><a href="science.html">Science</a></li></ul>');
+    
+    $("#contacts").html('<h3>Contact me</h3><p><a href="mailto:matthew@mjwasbrough.com" class="link"><i class="fa fa-at contact_icons"></i></a>&nbsp;<a href="https://twitter.com/MJWasbrough"><i class="fa fa-twitter-square contact_icons"></i></a>&nbsp;<a href="https://uk.linkedin.com/pub/matthew-wasbrough/45/3a/674"><i class="fa fa-linkedin-square contact_icons"></i></a></p>');
+
+});
