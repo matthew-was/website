360f18a6578476bc47446f13b0176d9699d77203
Matthew Wasbrough <matthew.was@gmail.com>
2018-06-17 10:19:16 +0100

    update for change from ecowebhosting to aws

--- live/common.js
+++ live/common.js
@@ -3,6 +3,6 @@ $(function () {
     
     $("#navigation").html('<div class="nav-container"><div class="navbar-header"><a href="index.html" class="navbar-brand">MJWasbrough.com</a><button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div><div class="collapse navbar-collapse"><ul class="nav navbar-nav"><li id="about"><a href="about.html">About Me</a></li><li id="webdev"><a href="programming.html">Programming</a></li><li id="science"><a href="science.html">Science</a></li></ul></div></div>');
 
-    $("#contacts").html('<h3>Contact me</h3><p><a href="mailto:matthew@mjwasbrough.com" class="link"><i class="fa fa-at contact_icons"></i></a>&nbsp;<a href="https://twitter.com/MJWasbrough"><i class="fa fa-twitter-square contact_icons"></i></a>&nbsp;<a href="https://uk.linkedin.com/pub/matthew-wasbrough/45/3a/674"><i class="fa fa-linkedin-square contact_icons"></i></a></p>');
+    $("#contacts").html('<h3>Contact me</h3><p><a href="mailto:web@wasbrough.com" class="link"><i class="fa fa-at contact_icons"></i></a>&nbsp;<a href="https://twitter.com/MJWasbrough"><i class="fa fa-twitter-square contact_icons"></i></a>&nbsp;<a href="https://uk.linkedin.com/pub/matthew-wasbrough/45/3a/674"><i class="fa fa-linkedin-square contact_icons"></i></a></p>');
 
 });
